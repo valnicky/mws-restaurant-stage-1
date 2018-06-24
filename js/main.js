7759e1b510fe49ae5885fa2b940fdcd663f040ef
Valeria <valery0007@gmail.com>
2018-06-24 22:01:25 +0200

    formating and last fixing

--- js/main.js
+++ js/main.js
@@ -181,24 +181,3 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 }
 
-//add service worker 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('../sw.js')
-  .then(reg => {
-    //registration worker
-    if(reg.installing) {
-      console.log('Service worker installing');
-    } else if (reg.waiting) {
-      console.log('Service worker installed');
-    } else if (reg.active) {
-      console.log('Service worker active');
-    }
-
-    console.log('Registration succeeded, Scope is ' + reg.scope);
-  }).catch(error => {
-    //registration failed
-    console.log('Registration failed with ' + error);
-  });
-
-
-}
--- js/register.js
+++ js/register.js
@@ -9,3 +9,26 @@ if('serviceWorker' in navigator) {
 		console.log("Registration failed" + error);
 	});
 }
+
+/*
+//add service worker 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../sw.js')
+  .then(reg => {
+    //registration worker
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if (reg.waiting) {
+      console.log('Service worker installed');
+    } else if (reg.active) {
+      console.log('Service worker active');
+    }
+
+    console.log('Registration succeeded, Scope is ' + reg.scope);
+  }).catch(error => {
+    //registration failed
+    console.log('Registration failed with ' + error);
+  });
+
+
+}*/
